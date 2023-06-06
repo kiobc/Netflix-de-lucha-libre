2ef622ac6b2e9dccd9a476d6c7cbb6f071baf958
kiobc <brayan.calderon3464@utc.edu.ec>
2023-06-05 22:22:24 -0500

    Acabado navbar

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,9 +1,17 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons"
 import "./navbar.scss"
+import React, { useState } from 'react';
+
 
 function Navbar() {
+  const[isScrolled,setIsScrolled]=useState(false);
+  window.onscroll=()=> {
+    setIsScrolled(window.pageYOffset===0?false:true);
+    return()=>(window.onscroll=null);
+  };
+  console.log(isScrolled);
   return (
-    <div className="navbar">
+    <div className={isScrolled? "navbar scrolled":"navbar" }>
         <div className="container">
             <div className="left">
                 <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" />
@@ -18,7 +26,15 @@ function Navbar() {
                 <span>KID</span>
                 <Notifications className="icon"/>
                 <img src="https://th.bing.com/th/id/R.b62b5ea41d675ad646b8da931c65fdff?rik=fGGfU4PQmEmmbg&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f1789%2f4287%2fproducts%2fUS-WWE-Stone-Cold-Steve-Austin-_Logo-01A-Black_1024x1024.jpg%3fv%3d1571678894&ehk=zd9Ldb1cbcowBG8tR%2baFfqapsgts3XT0D8jNNXWRT9g%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                <div className="perfil">
+
+                
                 <ArrowDropDown className="icon"/>
+                <div className="opciones">
+                  <span>Configuración</span>
+                  <span>Salir</span>
+                  </div>
+                </div>
                 </div>
 
             </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
+        <img width="100%" src="http://1.bp.blogspot.com/-kMaEHynunOg/UCIjI__73VI/AAAAAAAAAZE/03MO_DhSzzo/s1600/WWE+Events+Wallpaper+4.jpg" alt="" />
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ratione asperiores illo quidem eos iusto sequi quaerat mollitia consequuntur debitis, tenetur itaque sed esse ipsum obcaecati hic nisi natus quos.
     </div>
   )
 }
