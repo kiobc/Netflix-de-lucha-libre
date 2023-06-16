7dfb47cb71e6e2e8eea80e1124d7f6df26b50020
kiobc <brayan.calderon3464@utc.edu.ec>
2023-06-15 22:01:27 -0500

    arreglado la parte de trailer y mejorado la parte de desceripcion y botones

--- src/componentes/listaItem/ListaItem.jsx
+++ src/componentes/listaItem/ListaItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './listaItem.scss';
 import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
 
@@ -13,35 +14,40 @@ export default function ListaItem({ index }) {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img src="https://th.bing.com/th/id/R.af90d224c738702c6c9e18d7fd144bcf?rik=q%2bTASMIRzHw4ZA&riu=http%3a%2f%2fimages2.fanpop.com%2fimages%2fphotos%2f4600000%2fWrestleMania-XXV-professional-wrestling-4696205-1280-1024.jpg&ehk=WDX0EaqnR2%2fNnr6e%2bh00eixi%2boAmtU185pvKTnTNcfI%3d&risl=&pid=ImgRaw&r=0" alt="" />
-      {isHovered && (
-        <>
-          <iframe
-            src={`https://www.youtube.com/embed/${trailerId}?autoplay=1`}
-            title="YouTube Video"
-            allowFullScreen
+      {isHovered ? (
+        <div className="videoWrapper">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailerId}`}
+            className="react-player"
+            width="100%"
+            height="100%"
+            controls
+            playing={isHovered}
           />
-          <div className="itemInfo">
-            <div className="iconos">
-              <PlayArrow />
-              <Add />
-              <ThumbUpAltOutlined />
-              <ThumbDownOutlined />
-            </div>
-            <div className="itemInfoTop">
-              <span>1 hora 14 min</span>
-              <span className="limite">+16</span>
-              <span>1999</span>
-            </div>
-            <div className="descripcion">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis necessitatibus explicabo maiores dicta iusto iure quasi aliquam quis tempore sed odit cumque voluptatem repellat dignissimos, nam rerum nostrum voluptatibus.
-            </div>
-            <div className="genero">
-              Acción
-            </div>
-          </div>
-        </>
+        </div>
+      ) : (
+        <img src="https://th.bing.com/th/id/R.af90d224c738702c6c9e18d7fd144bcf?rik=q%2bTASMIRzHw4ZA&riu=http%3a%2f%2fimages2.fanpop.com%2fimages%2fphotos%2f4600000%2fWrestleMania-XXV-professional-wrestling-4696205-1280-1024.jpg&ehk=WDX0EaqnR2%2fNnr6e%2bh00eixi%2boAmtU185pvKTnTNcfI%3d&risl=&pid=ImgRaw&r=0" alt="" />
       )}
+
+      <div className="itemInfo">
+        <div className="iconos">
+          <PlayArrow className='icono' />
+          <Add className='icono'/>
+          <ThumbUpAltOutlined className='icono' />
+          <ThumbDownOutlined className='icono'/>
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hora 14 min</span>
+          <span className="limite">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="descripcion">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis necessitatibus explicabo maiores dicta iusto iure quasi aliquam quis tempore sed odit .
+        </div>
+        <div className="genero">
+          Acción
+        </div>
+      </div>
     </div>
   );
 }
