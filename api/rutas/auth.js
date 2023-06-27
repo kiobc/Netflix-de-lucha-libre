f2a5b7a98fc8965c22b1c2b17afeba9ad289de15
kiobc <brayan.calderon3464@utc.edu.ec>
2023-06-26 22:25:17 -0500

    Agregado la parte de login a la api

--- api/rutas/auth.js
+++ api/rutas/auth.js
@@ -1,6 +1,7 @@
 const router= require('express').Router();
 const Usuario=require('../modelos/Usuario');
 const CryptoJS=require('crypto-js');
+const jwt=require('jsonwebtoken');
 
 // Registro
 router.post("/registro",async (req,res)=>{
@@ -16,4 +17,28 @@ res.status(200).json(usuario);
     res.status(500).json(err);
 }
 });
+// Login
+router.post("/login", async (req, res) => {
+    try {
+      const usuario = await Usuario.findOne({ email: req.body.email });
+      if (!usuario) {
+        return res.status(401).json("Usuario o contraseña incorrectos");
+      }
+  
+      const bytes = CryptoJS.AES.decrypt(usuario.password.toString(), process.env.SECRET_KEY);
+      const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+  
+      if (originalPassword !== req.body.password) {
+        return res.status(401).json("Usuario o contraseña incorrectos");
+      }
+      const accessToken = jwt.sign({ id: usuario._id, isAdmin: usuario.isAdmin },
+         process.env.SECRET_KEY, { expiresIn: "5d"});
+      const { password, ...info } = usuario._doc;
+  
+      res.status(200).json({...info, accessToken});
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 module.exports=router;
